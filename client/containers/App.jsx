1c25f8aeee5a9fafc1f64a7f4ea8f37723f04ef1
arshiamasih <arshia.masih@gmail.com>
2019-07-04 18:47:59 -0700

    pwa working with react

---
+++ client/containers/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { render } from "react-dom";
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+      <h1> Hello World (react) </h1>
+     </div>
+    )
+  }
+
+}
+
+export default App;
