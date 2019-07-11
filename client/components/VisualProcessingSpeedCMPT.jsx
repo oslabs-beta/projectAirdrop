1cc0970ac03590d57348db80ca85dde1cdb79543
codesmith <mshermandev@gmail.com>
2019-07-11 12:12:33 -0700

    Displays Answer Choices, properly hides buttons, stops timers after series completion

--- client/components/VisualProcessingSpeedCMPT.jsx
+++ client/components/VisualProcessingSpeedCMPT.jsx
@@ -4,15 +4,32 @@ import UserStartBTN from './UserStartBTN.jsx';
 const VisualProcessingSpeed = (props) => {
   let currentEl;
   let currentBTN;
+  let choiceDisplay = [];
   if(props.timerRunning) currentEl = props.vpsAnswers[0][props.currentSeriesIndex][props.currentElementIndex];
-  if(props.practiceRun) currentBTN = <button onClick={props.startPractice}>Start</button>
-  else currentBTN = <button onClick={props.startNewSeries}>Start</button>
+  if(!props.displayingAnswers) {
+    if(!props.practiceDone) currentBTN = <button onClick={props.startPractice}>Start</button>
+    else currentBTN = <button onClick={props.startNewSeries}>Start</button>
+  }
+  console.log(props.displayingAnswers, 'displaying answers')
+  if(props.displayingAnswers){
+    for(let j = 0; j < 4; j++){
+      let choiceRow = [];      
+      for(let i = 0; i < 5; i++){
+        choiceRow.push(<div>{props.vpsAnswers[j][props.currentSeriesIndex][i]}</div>)
+      }
+      console.log(choiceRow)
+      choiceDisplay.push(<span>
+        {choiceRow}
+        </span>)
+    }
+  }
   console.log(currentEl);
   return (
   <div>
   <h1>VisualProcessingSpeed</h1>
    {currentEl}
    {currentBTN}
+   {choiceDisplay}
   </div>
 )};
 
