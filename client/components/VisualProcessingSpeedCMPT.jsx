94ce6a783833ca43b9ccb69bc33ef6aa8541f4f4
codesmith <mshermandev@gmail.com>
2019-07-11 12:30:59 -0700

    De-Hotel California'd VPS

--- client/components/VisualProcessingSpeedCMPT.jsx
+++ client/components/VisualProcessingSpeedCMPT.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import UserStartBTN from './UserStartBTN.jsx';
+import UserNextBtn from './UserNextBTN.jsx';
 
 const VisualProcessingSpeed = (props) => {
   let currentEl;
   let currentBTN;
   let choiceDisplay = [];
   if(props.timerRunning) currentEl = props.vpsAnswers[0][props.currentSeriesIndex][props.currentElementIndex];
-  if(!props.displayingAnswers) {
+  if(!props.displayingAnswers && !props.timerRunning) {
     if(!props.practiceDone) currentBTN = <button onClick={props.startPractice}>Start</button>
     else currentBTN = <button onClick={props.startNewSeries}>Start</button>
   }
+  if(props.testDone) currentBTN = <UserNextBtn changeSection={props.changeSection} />;
   console.log(props.displayingAnswers, 'displaying answers')
   if(props.displayingAnswers){
     for(let j = 0; j < 4; j++){
