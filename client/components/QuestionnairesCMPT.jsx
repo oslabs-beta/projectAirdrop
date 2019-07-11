cdd3e8c5ac078263f2c10a5706768adfb41d6033
aaron <aaronmillersliveaccount@live.com>
2019-07-11 12:22:57 -0700

    questionnaires mvp done

--- client/components/QuestionnairesCMPT.jsx
+++ client/components/QuestionnairesCMPT.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import QuestionnairesBTN from './QuestionnaireBTN';
 
-const Questionnaires = () => (
-  <h1>Questionnaires</h1>
-);
 
-export default Questionnaires;
+const QuestionnairesCMPT = (props) => {
+  const questions = [];
+
+  for (let i = 0; i < props.questions.length; i++) {
+  questions.push(
+  <li 
+  key={new Date().getTime()+i}
+  
+  >
+    {props.questions[i].question_text}
+    <QuestionnairesBTN 
+    qid={props.questions[i].id}
+    questionnaire={props.questions.length > 12 ? 'CMSQ' : 'CNAAQ'}
+    />
+  </li>)
+}
+
+
+  
+
+  console.log('q', props)
+  return (
+    <ul>
+      {questions}
+    </ul>
+  );
+};
+
+export default QuestionnairesCMPT;
+
