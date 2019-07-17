f1293561f20c28dadf35d37e77d3c1ff73cb61ec
arshiamasih <arshia.masih@gmail.com>
2019-07-16 20:08:15 -0700

    updated back end file structure

--- server/controllers/encryptionController.js
+++ server/controllers/encryptionController.js
@@ -9,38 +9,37 @@ module.exports = {
       if (err) {
         return next(err);
       }
-      bcrypt.hash(req.body.pwd, salt, (err, hash) => {
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
         if (err) {
           return next(err);
         }
-        req.body.pwd = hash;
+        req.body.password = hash;
         next();
       });
     });
-  },
-
+  }
 
   //does this need to be formatted like databaseController in promises 
-  comparePassword(req, res, next) {
-    const queryString = 'SELECT * FROM users WHERE username = $1';
-    const values = [req.body.data.username];
-    db.query(queryString, values, (err, result) => {
-      if (err || !result.rows[0]) {
-        return next(err);
-      }
-      bcrypt.compare(req.body.data.password, result.rows[0].password, (err, isMatch) => {
-        if (err) {
-          return next(err);
-        }
-        if (isMatch) {
-          res.locals.encryptedPassword = result.rows[0].password;
-          return next();
-        }
+  // comparePassword(req, res, next) {
+  //   const queryString = 'SELECT * FROM users WHERE username = $1';
+  //   const values = [req.body.data.username];
+  //   db.query(queryString, values, (err, result) => {
+  //     if (err || !result.rows[0]) {
+  //       return next(err);
+  //     }
+  //     bcrypt.compare(req.body.data.password, result.rows[0].password, (err, isMatch) => {
+  //       if (err) {
+  //         return next(err);
+  //       }
+  //       if (isMatch) {
+  //         res.locals.encryptedPassword = result.rows[0].password;
+  //         return next();
+  //       }
         
-        return next('wrong password entered');
-      });
-    });
-  }
+  //       return next('wrong password entered');
+  //     });
+  //   });
+  // }
 }
 
 
---
+++ server/controllers/userController.js
@@ -0,0 +1,33 @@
+//post at sign up
+//login
+//compare password 
+
+const userModel = require('../models/userModel');
+const userController = {};
+
+userController.comparePassword = (req,res,next) => {
+
+
+}
+
+
+// comparePassword(req, res, next) {
+//   const queryString = 'SELECT * FROM users WHERE username = $1';
+//   const values = [req.body.data.username];
+//   db.query(queryString, values, (err, result) => {
+//     if (err || !result.rows[0]) {
+//       return next(err);
+//     }
+//     bcrypt.compare(req.body.data.password, result.rows[0].password, (err, isMatch) => {
+//       if (err) {
+//         return next(err);
+//       }
+//       if (isMatch) {
+//         res.locals.encryptedPassword = result.rows[0].password;
+//         return next();
+//       }
+      
+//       return next('wrong password entered');
+//     });
+//   });
+// }
