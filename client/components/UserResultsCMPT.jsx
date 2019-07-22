04644ea959815f8cbbf9c662a73c172f176e153d
Charlie Miller <charliemiller@Charlies-MacBook-Pro.local>
2019-07-22 10:12:07 -0700

    added results cmpt

--- client/components/UserResultsCMPT.jsx
+++ client/components/UserResultsCMPT.jsx
@@ -5,14 +5,18 @@ const UserResults = props => {
   let vpsCorrectNumber = 0;
   let wmCorrectNumber = 0;
   let irCorrectNumber = 0;
-  let wordArr = props.ltvr.words.reduce((accum, el, index) => {
-    return accum.push(el)
-  }, [])
+  let wordArr = props.ltvr.words.reduce((acc, el, index) => {
+    return acc.push(el)
+  }, []);
   for(let i = 0; i < wordArr.length; i++){
     for(let j = 0; j < props.ltvr.responses.length; j++){
       if(wordArr[i] = props.ltvr.responses[j]) ltvrCorrectNumber++;
     }
   }
+  vpsCorrectNumber = props.vps.responses.reduce((acc, el, index) => {
+    if(el === '1') return acc + 1;
+    return acc;
+  })
   return (
     <div>
       {ltvrCorrectNumber}
